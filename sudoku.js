7aff703c4bce96e6073792d8a765738811d74a74
afifahrahmak <afifahrk96@gmail.com>
2019-09-07 00:29:17 +0700

    pseudoku

--- sudoku.js
+++ sudoku.js
@@ -9,72 +9,6 @@ class Sudoku {
     this.board(this.str)
   }
 
-  checkHorizontal(row,value){
-    for (let j=0; j<this.boardSudoku.length; j++){
-      if (this.boardSudoku[row][j] == value){
-          return false
-      }
-    }
-    return true;
-  }
-
-  checkVertical(col, value){
-    for (let i=0; i<this.boardSudoku.length;  i++){
-      if (this.boardSudoku[i][col] == value){
-        return false
-      }
-    }
-    return true
-  }
-
-  checkBox(row,col,value){
-    let X = Math.floor(row/3)*3
-    let Y = Math.floor(col/3)*3
-    for (let i=X; i<X+3; i++) {
-      for (let j=Y; j<Y+3; j++) {
-        if (this.boardSudoku[i][j] == value){
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  checkAll(col, row, value){
-    if (this.checkVertical(row, value) && this.checkHorizontal(col, value) && this.checkBox(col, row, value)){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-
-  solve() {
-    for (let i=0; i<this.empty.length; i++){
-      for (let k=1; k<10 ; k++){
-        if (this.checkAll(this.empty[i][0], this.empty[i][1], k)){
-          this.history.push({
-            row: this.empty[i][0],
-            col: this.empty[i][1],
-            val: k,
-            index: i
-          })
-          this.boardSudoku[this.empty[i][0]][this.empty[i][1]] = String(k);
-          break
-        }
-        if (k === 9){
-          k = this.history[this.history.length-1].val
-          let row = this.history[this.history.length-1].row
-          let col = this.history[this.history.length-1].col
-          i = this.history[this.history.length-1].index
-          this.boardSudoku[row][col] = ' '
-          this.history.pop();
-        }
-      }
-    }
-    console.log(this.boardSudoku);
-  }
-
   // Returns a string representing the current state of the board
   board(str) {
     let count = 0
@@ -92,6 +26,77 @@ class Sudoku {
       }
     }
   }
+  checkHorizontal(row,value){
+    for (let j=0; j<this.boardSudoku.length; j++){
+      if (this.boardSudoku[row][j] == value){
+          return false
+      }
+    }
+    return true;
+  }
+  checkVertical(col, value){
+    for (let i=0; i<this.boardSudoku.length;  i++){
+      if (this.boardSudoku[i][col] == value){
+        return false
+      }
+    }
+    return true
+  }
+  checkBox(row,col,value){
+    let X = Math.floor(row/3)*3
+    let Y = Math.floor(col/3)*3
+    for (let i=X; i<X+3; i++) {
+      for (let j=Y; j<Y+3; j++) {
+        if (this.boardSudoku[i][j] == value){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  checkAll(col, row, value){
+    if (this.checkVertical(row, value) && this.checkHorizontal(col, value) && this.checkBox(col, row, value)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  solve() {
+    for (let i=0; i<this.empty.length; i++){
+      for (let k=1; k<10 ; k++){
+        if (this.checkAll(this.empty[i][0], this.empty[i][1], k)){
+          this.history.push({
+            row: this.empty[i][0],
+            col: this.empty[i][1],
+            val: k,
+            index: i
+          })
+          this.boardSudoku[this.empty[i][0]][this.empty[i][1]] = String(k);
+          break
+        }
+        while (k >= 9){
+          k = this.history[this.history.length-1].val
+          let row = this.history[this.history.length-1].row
+          let col = this.history[this.history.length-1].col
+          i = this.history[this.history.length-1].index
+          this.boardSudoku[row][col] = ' '
+          this.history.pop();
+        }
+      }
+      console.clear()
+      console.log(this.boardSudoku);
+      this.sleep(250)
+    }
+  }
+  sleep (milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+        break;
+        }
+    }
+  }
 }
 
 // The file has newlines at the end of each line,
@@ -106,4 +111,4 @@ var game = new Sudoku(board_string)
 // Remember: this will just fill out what it can and not "guess"
 game.solve();
 
-console.log(game.board())
+// console.log(game.board())
