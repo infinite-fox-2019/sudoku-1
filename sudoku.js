e088ec93a55b4211a7162c0ab5d8d1668909e297
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-06 18:30:02 +0700

    3x3 Checker done

--- sudoku.js
+++ sudoku.js
@@ -3,32 +3,68 @@
 class Sudoku {
   constructor(board_string) {}
 
-  solve() {}
 
-  // Returns a string representing the current state of the board
-  board() {
-    let board = []
-    let counter = 0
-    let temp = []
-    for (let i = 0; i < board_string.length; i++) {
-      if (counter < 8){
-        temp.push(board_string[i])
-        counter++
-      } else {
-        temp.push(board_string[i])
-        board.push(temp)
-        temp = []
-        counter = 0
-      }
-    }
-    for (let i = 0; i< board.length; i++){
-      for (let j = 0; j< board[i].length; j++){
-        if (board[i][j]==="0"){
-          board[i][j] = " "
+  solve() {
+    let board = this.board();
+    
+    let tracking = {
+      i : '',
+      j : '',
+      value : ''
+    } 
+
+
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === " ") {
+          for (let k = 1; k < 10; k++) {
+            if (this.checkHorizontal(k, i, j, board)) {
+              board[i][j] = String(k);
+            }
+          }
         }
       }
     }
-    console.log(board)
+    // break; 
+    console.log(board);
+  }
+
+  checkHorizontal(input, row, col, board) {
+    for (let i = 0; i < 9; i++) {
+      let ver = Math.floor(row/3)*3 + Math.floor(i/3);
+      let hor = Math.floor(col/3)*3 + i%3;
+
+      if (board[row][i] == input || board[i][col] == input || board[ver][hor] == input) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Returns a string representing the current state of the board
+  board() {
+    let board = [];
+    let counter = 0;
+    let temp = [];
+    for (let i = 0; i < board_string.length; i++) {
+      if (counter < 8) {
+        temp.push(board_string[i]);
+        counter++;
+      } else {
+        temp.push(board_string[i]);
+        board.push(temp);
+        temp = [];
+        counter = 0;
+      }
+    }
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === "0") {
+          board[i][j] = " ";
+        }
+      }
+    }
+    return board;
   }
 }
 
@@ -44,5 +80,5 @@ var game = new Sudoku(board_string);
 
 // Remember: this will just fill out what it can and not "guess"
 game.solve();
-
-console.log(game.board());
+// game.numberGenerator();
+// console.log(game.board());
