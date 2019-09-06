8ef43264b4d39ceb56a13264f8de49a9c82eeaf2
uzaisindiko <uzai.sindiko@gmail.com>
2019-09-06 21:16:17 +0700

    sudoku

--- sudoku.js
+++ sudoku.js
@@ -1,24 +1,140 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.board_string = board_string
+    this.board = this.makeBoard()
+    this.printBoard = this.printBoard()
+  }
 
-  solve() {}
+  makeBoard() {
+    let output = []
+    let count = 0
+    for (let i = 0; i < 9; i++) {
+      let row = []
+      for (let j = 0; j < 9; j++) {
+        row.push(Number(this.board_string[count]))
+        count++
+      }
+      output.push(row)
+    }
 
-  // Returns a string representing the current state of the board
-  board() {}
+
+    return output
+  }
+
+  solve() {
+
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        if (this.board[i][j] === 0) {
+
+
+          for (let num = 0; num < 10; num++) {
+            if (this.checkAll(num, i, j)) {
+              this.board[i][j] = num
+              if (this.solve()) {
+                return true
+              } else {
+                this.board[i][j] = 0
+              }
+            }
+          }
+          return false
+
+
+        }
+
+
+
+
+      }
+    }
+    return true
+
+
+  }
+
+  checkRow(num, coorI) {
+
+    for (let j = 0; j < this.board.length; j++) {
+      if (this.board[coorI][j] === num) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  checkCol(num, coorJ) {
+
+    for (let i = 0; i < this.board.length; i++) {
+      if (this.board[i][coorJ] === num) {
+        return false
+      }
+    }
+    return true
+  }
+
+  checkBox(num, coorI, coorJ) {
+
+    for (let i = Math.floor(coorI / 3) * 3; i < 3; i++) {
+      for (let j = Math.floor(coorJ / 3) * 3; j < 3; j++) {
+        if (this.board[i][j] == num) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+
+  checkAll(num, coorI, coorJ) {
+    if (this.checkRow(num, coorI) && this.checkCol(num, coorJ) && this.checkBox(num, coorI, coorJ)) {
+      return true
+    }
+    return false
+  }
+
+
+  printBoard() {
+    this.solve()
+    let output = '=================================\n'
+    output += '=======_______SUDOKU_______======\n'
+    output += '=================================\n'
+
+
+    this.board.forEach((el, i) => {
+      output += el.join(' | ')
+      output += '\n'
+      if ((i + 1) % 3 === 0) {
+        for (let j = 0; j < 33; j++) {
+          output += '-'
+        }
+        output += '\n'
+      }
+    })
+
+    return output
+  }
+
+
 }
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
 var fs = require('fs')
-var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
-  .toString()
-  .split("\n")[0]
 
-var game = new Sudoku(board_string)
+for (let i = 0; i < 20; i++) {
+  var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
+    .toString()
+    .split("\n")[0]
+
+  var game = new Sudoku(board_string)
+  console.log(`--------------(${i + 1})----------------`)
+  console.log(game.printBoard);
+  console.log('\n');
+}
+
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
-
-console.log(game.board())
